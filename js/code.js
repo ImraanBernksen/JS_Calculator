a12ea65ff7b3c58c21e936d6ef5ef3d0d8bac59a
ImraanBernksen <imraanbernksen@gmail.com>
2022-11-26 10:06:16 +0200

    done

--- js/code.js
+++ js/code.js
@@ -1,13 +1,34 @@
 function clearScreen() {
     document.getElementById("result").value = "";
+    allowDecimal = true;
 }
 
 function display(value) {
     document.getElementById("result").value += value;
 }
 
+function displayOperator(value) {
+    document.getElementById("result").value += value;
+    allowDecimal = true;
+}
+
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
+    if (document.getElementById("result").value.includes('.')) {
+        allowDecimal = false;
+    } else {
+        allowDecimal = true;
+    }
 }
+
+let allowDecimal = true;
+
+function displayDecimal(value) {
+    if (allowDecimal) {
+        document.getElementById("result").value += value;
+        allowDecimal = false;    
+    }
+}
+
